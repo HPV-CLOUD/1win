b10899c4d81593401b4bcc69c8eee074c7860930
HPV-CLOUD <tgprems444@gmail.com>
2024-09-16 10:46:20 +0500

    Add files via upload

---
+++ HPV_1win.py
@@ -0,0 +1,10 @@
+from HPV_TEAM_1win import HPV_Main
+
+from Core.Config.HPV_Config import *
+
+
+
+if __name__ == '__main__':
+    HPV_Main(COINS, MAX_LVL, MAX_ENERGY_LIMIT, MAX_ENERGY_REGEN, AUTO_UPDATE)
+
+
